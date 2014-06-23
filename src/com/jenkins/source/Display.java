bfd7cdf99e2b51778f7ab9b7645369d99af6d23c
ashwanisinghTN <singhrahul27@gmail.com>
2014-06-23 20:51:20 +0530

    this is my first commit

---
+++ src/com/jenkins/source/Display.java
@@ -0,0 +1,14 @@
+package com.jenkins.source;
+
+public class Display {
+
+	/**
+	 * @param args
+	 */
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Message().sayHello("Hello Jenkins");
+	}
+
+}
---
+++ src/com/jenkins/source/Message.java
@@ -0,0 +1,9 @@
+package com.jenkins.source;
+
+public class Message {
+	
+	public static void sayHello(String mess){
+		System.out.println(mess);
+	}
+
+}
