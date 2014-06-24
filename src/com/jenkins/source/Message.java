501c0f646bd8ac90f0b0ba9528ed8ddcb619733d
ashwanisinghTN <singhrahul27@gmail.com>
2014-06-24 10:45:50 +0530

    Second commit

--- src/com/jenkins/source/Message.java
+++ src/com/jenkins/source/Message.java
@@ -5,5 +5,9 @@ public class Message {
 	public static void sayHello(String mess){
 		System.out.println(mess);
 	}
+	
+	public static void sayHelloAgain(String mess1){
+		System.out.println("This will good:"+mess1);
+	}
 
 }
